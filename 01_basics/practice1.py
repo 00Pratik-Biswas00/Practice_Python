e974b0c09bf1a93ff16ff1fd1b4d061e9dc6be7c
00Pratik-Biswas00 <04biswaspratik@gmail.com>
2025-09-15 18:10:55 +0530

    practising

---
+++ 01_basics/practice1.py
@@ -0,0 +1,22 @@
+a=6
+b=4
+c='&'
+
+print(a*str(a), c, b*str(b))
+print(a*str(a) + c + b*str(b))
+
+print(int(10.23))
+
+print(int("3")*2)
+
+a, b = b, a
+print(a, b)
+
+p=2
+r=2
+n=10
+print(p*r**(n-1))
+
+print(~(~2))
+
+print(abs(-2))
